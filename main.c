056a8edf2a7082762599008ba86198fd1a99a449
Guilherme Sanches Cavazztto <guicavazzotto@gmail.com>
2025-04-25 10:35:08 -0300

    Adicionado 180 graus e mudança no efeito raio-x

--- main.c
+++ main.c
@@ -80,8 +80,8 @@ void efeitoRaioX(Imagem *img) {
     float fator = 1.5;
     for (int j = 0; j < img->altura; j++) {
         for (int i = 0; i < img->largura; i++) {
-            int cinza = (int)(img->R[j][i]*0.299 + img->G[j][i]*0.587 + img->B[j][i]*0.114);
-            int raioX = (int)(pow(cinza, fator));
+            int cinza = (int)(img->R[j][i] * 0.299 + img->G[j][i] * 0.587 + img->B[j][i] * 0.114);
+            int raioX = (int)(pow(cinza / 255.0, fator) * 255);
             if (raioX > 255) raioX = 255;
             img->R[j][i] = img->G[j][i] = img->B[j][i] = raioX;
         }
@@ -118,7 +118,7 @@ void rotacionarImagem(Imagem *img) {
     int **R_temp = malloc(novaAltura * sizeof(int *));
     int **G_temp = malloc(novaAltura * sizeof(int *));
     int **B_temp = malloc(novaAltura * sizeof(int *));
-    
+
     for (int i = 0; i < novaAltura; i++) {
         R_temp[i] = malloc(novaLargura * sizeof(int));
         G_temp[i] = malloc(novaLargura * sizeof(int));
@@ -142,15 +142,33 @@ void rotacionarImagem(Imagem *img) {
     img->B = B_temp;
 }
 
+void rotacionar180(Imagem *img) {
+    for (int j = 0; j < img->altura / 2; j++) {
+        for (int i = 0; i < img->largura; i++) {
+            int inv_j = img->altura - 1 - j;
+            int inv_i = img->largura - 1 - i;
+
+            int tempR = img->R[j][i];
+            int tempG = img->G[j][i];
+            int tempB = img->B[j][i];
+
+            img->R[j][i] = img->R[inv_j][inv_i];
+            img->G[j][i] = img->G[inv_j][inv_i];
+            img->B[j][i] = img->B[inv_j][inv_i];
+
+            img->R[inv_j][inv_i] = tempR;
+            img->G[inv_j][inv_i] = tempG;
+            img->B[inv_j][inv_i] = tempB;
+        }
+    }
+}
+
 void efeitoPretoBranco(Imagem *img) {
     for (int j = 0; j < img->altura; j++) {
         for (int i = 0; i < img->largura; i++) {
             int media = (img->R[j][i] + img->G[j][i] + img->B[j][i]) / 3;
-            if (media < 128)
-                media = 0;    // preto
-            else
-                media = 255;  // branco
-            img->R[j][i] = img->G[j][i] = img->B[j][i] = media;
+            int valor = (media > 127) ? 255 : 0;
+            img->R[j][i] = img->G[j][i] = img->B[j][i] = valor;
         }
     }
 }
@@ -158,7 +176,7 @@ void efeitoPretoBranco(Imagem *img) {
 void efeitoTonsDeCinza(Imagem *img) {
     for (int j = 0; j < img->altura; j++) {
         for (int i = 0; i < img->largura; i++) {
-            int cinza = (int)(img->R[j][i] * 0.3 + img->G[j][i] * 0.59 + img->B[j][i] * 0.11);
+            int cinza = (int)(img->R[j][i] * 0.299 + img->G[j][i] * 0.587 + img->B[j][i] * 0.114);
             img->R[j][i] = img->G[j][i] = img->B[j][i] = cinza;
         }
     }
@@ -183,7 +201,8 @@ int main() {
         printf("5. Efeito preto e branco\n");
         printf("6. Efeito tons de cinza\n");
         printf("7. Salvar imagem\n");
-        printf("8. Sair\n");
+        printf("8. Girar 180 graus\n");
+        printf("9. Sair\n");
         printf("Escolha uma opção: ");
         scanf("%d", &opcao);
 
@@ -202,10 +221,11 @@ int main() {
                 printf("Imagem salva com sucesso!\n");
                 break;
             }
-            case 8: printf("Encerrando...\n"); break;
+            case 8: rotacionar180(&img); break;
+            case 9: printf("Encerrando...\n"); break;
             default: printf("Opção inválida.\n");
         }
-    } while (opcao != 8);
+    } while (opcao != 9);
 
     liberarImagem(&img);
     return 0;
